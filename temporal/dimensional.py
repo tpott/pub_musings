7d617a1fbed7c6f47fb5236fcf5cb9baffef4bce
Trevor Pottinger <tpott3@gmail.com>
2022-03-07 23:21:53 -0800

    more tools

---
+++ temporal/dimensional.py
@@ -0,0 +1,244 @@
+# dimensional.py
+# Trevor Pottinger
+# Mon Mar  7 20:57:47 PST 2022
+
+import datetime
+import http.server
+import random
+import secrets
+import socketserver
+import ssl
+import sys
+import traceback
+import urllib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from deseasonalize import deseasonalize
+
+
+EXTRA_WIDE = [12.8, 4.8]
+HOSTNAME = "localhost"
+PORT = 8000
+YMD_FORMAT = "%Y-%m-%d"
+
+
+# This class is necessary to use the below syntax of `with Server(..) as ..`
+class TCPServer(socketserver.TCPServer):
+  def __enter__(self):
+    return self
+
+  def __exit__(self, *args):
+    self.server_close()
+
+
+# primary entry point is through do_GET
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+
+  def __init__(self, *args, **kwargs):
+    # Runs once for every request
+    super().__init__(*args, **kwargs)
+
+  def getStatusPage(self):
+    s = b"1-AM-ALIVE"
+    self.send_response(http.server.HTTPStatus.OK)
+    # Other potentially good headers: Content-type, Last-Modified
+    self.send_header("Content-Length", len(s))
+    self.send_header("Content-Type", "text/html; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(s)
+    return
+
+  def getIndexPage(self):
+    page = (
+      "<html>\n"
+      "<head>\n"
+      f"<meta http-equiv=\"refresh\" content=\"0;url=https://{HOSTNAME}:{PORT}/explore?n=1&start=2004-01-01&ticker=AAPL\" />\n"
+      "</head>\n"
+      "</html>"
+    )
+    s = page.encode("utf-8")
+    self.send_response(http.server.HTTPStatus.OK)
+    # Other potentially good headers: Content-type, Last-Modified
+    self.send_header("Content-Length", len(s))
+    self.send_header("Content-Type", "text/html; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(s)
+    return
+
+  def getNextPage(self, inc):
+    request = urllib.parse.urlparse(self.path)
+    query = urllib.parse.parse_qs(request.query)
+    n = query["n"][0]
+    ticker = query["ticker"][0]
+    start = datetime.datetime.strptime(query["start"][0], YMD_FORMAT)
+    next_start = (start + datetime.timedelta(days=inc * 30)).strftime(YMD_FORMAT)
+    self.send_response(http.server.HTTPStatus.FOUND)
+    self.send_header("Location", f"https://{HOSTNAME}:{PORT}/explore?n={n}&start={next_start}&ticker={ticker}")
+    self.end_headers()
+    return
+
+  def getExplorePage(self):
+    request = urllib.parse.urlparse(self.path)
+    query = urllib.parse.parse_qs(request.query)
+    n = int(query["n"][0])
+    ticker = query["ticker"][0]
+    start = datetime.datetime.strptime(query["start"][0], YMD_FORMAT)
+    end = (start + datetime.timedelta(days=2 * 365)).strftime(YMD_FORMAT)
+    img_url = f"https://{HOSTNAME}:{PORT}/image?n={n}&start={query['start'][0]}&ticker={ticker}&end={end}"
+
+    page = """<html>
+  <head>
+    <title>Explore</title>
+    <script>
+
+// https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
+function checkKey(e) {
+  let sp = new URLSearchParams(window.location.search);
+  let n = parseInt(sp.get('n'));
+  console.log(n);
+  e = e || window.event;
+  console.log(e.keyCode);
+  if (e.keyCode === 37) {
+    // left arrow
+    window.location = '/prev?' + sp.toString();
+    return;
+  } else if (e.keyCode === 38) {
+    // up arrow
+    sp.set('n', n + 1);
+    console.log(n + 1);
+  } else if (e.keyCode === 39) {
+    // right arrow
+    window.location = '/next?' + sp.toString();
+    return;
+  } else if (e.keyCode === 40) {
+    // down arrow
+    sp.set('n', Math.max(0, n - 1));
+    console.log(n - 1);
+  }
+  window.location.search = sp.toString();
+}
+
+document.onkeydown = checkKey;
+    </script>
+  </head>
+  <body>
+    <span>Hi</span>
+    <div><img src="%s" /></div>
+  </body>
+</html>""" % img_url
+    s = page.encode("utf-8")
+    self.send_response(http.server.HTTPStatus.OK)
+    # Other potentially good headers: Content-type, Last-Modified
+    self.send_header("Content-Length", len(s))
+    self.send_header("Content-Type", "text/html; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(s)
+    return
+
+  def getImagePage(self):
+    request = urllib.parse.urlparse(self.path)
+    query = urllib.parse.parse_qs(request.query)
+    n = int(query["n"][0])
+    start = query["start"][0]
+    end = query["end"][0]
+    ticker = query["ticker"][0]
+    self.send_response(http.server.HTTPStatus.OK)
+    self.send_header("Content-Type", "image/png")
+    self.end_headers()
+
+    # TODO pass filename into MyHandler
+    df = pd.read_csv("combined_a392a33ee14c7203.csv")
+    df = df[(df.Name == ticker) & (start <= df.Date) & (df.Date < end)]
+    # TODO pass metric into MyHandler
+    metric = "Close"
+    date_lambda = lambda x: pd.Series({"Values": dict(zip(x.Name, x[metric]))})
+    date_df = df.groupby("Date").apply(date_lambda)
+    big_df = pd.DataFrame(date_df.Values.values.tolist(), index=date_df.index)
+    norm_arr = deseasonalize(n, big_df.transpose().values)
+    norm_df = pd.DataFrame(np.transpose(norm_arr), index=big_df.index, columns=big_df.columns)
+    # big_df = big_df.dropna(axis=0, how="any")
+    axes = big_df[[ticker]].plot(figsize=EXTRA_WIDE)
+    norm_df[[ticker]].plot(ax=axes)
+    plt.legend(["big_df", "norm_df"])
+    plt.savefig(self.wfile, format="png")
+    return
+
+  def getHandler(self):
+    request = urllib.parse.urlparse(self.path)
+    if request.path == "/":
+      self.getIndexPage()
+      return
+    if request.path == "/status":
+      self.getStatusPage()
+      return
+    if request.path == "/explore":
+      self.getExplorePage()
+      return
+    if request.path == "/next":
+      self.getNextPage(1)
+      return
+    if request.path == "/prev":
+      self.getNextPage(-1)
+      return
+    if request.path == "/image":
+      self.getImagePage()
+      return
+    s = b"Unknown page"
+    self.send_response(http.server.HTTPStatus.NOT_FOUND)
+    self.send_header("Content-Length", len(s))
+    self.send_header("Content-Type", "text/html; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(s)
+    return
+
+  def do_GET(self):
+    # This gets called because this is a http.server.SimpleHTTPRequestHandler
+    # parse_request calls http.client.parse_headers and sets self.headers
+    s, e_type, e, trace = None, None, None, None
+    try:
+      self.getHandler()
+    except Exception:
+      e_type, e, trace = sys.exc_info()
+      # TODO add stack trace and exception message
+      s = b"<div>Unexpected exception, %s: %s</div>\n" % (e_type.__name__.encode("utf-8"), str(e).encode("utf-8"))
+      s += "\n".join(map(lambda s: "<div>%s</div>" % s, traceback.format_tb(trace))).encode("utf-8")
+    finally:
+      del e_type, e, trace
+    if s is None:
+      return
+    self.send_response(http.server.HTTPStatus.INTERNAL_SERVER_ERROR)
+    self.send_header("Content-Length", len(s))
+    self.send_header("Content-Type", "text/html; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(s)
+    return
+
+
+def main() -> None:
+  seed = secrets.randbits(64)
+  print(f"Seeded with: {seed}")
+  random.seed(seed)
+  
+  # This avoids "Address already in use" errors while testing
+  TCPServer.allow_reuse_address = True
+  with TCPServer(("0.0.0.0", PORT), MyHandler) as httpd:
+    print(f"Serving at {PORT}")
+    # Generate a self signed cert by running the following:
+    # `openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 30`
+    httpd.socket = ssl.wrap_socket(
+      httpd.socket,
+      server_side=True,
+      keyfile="key.pem",
+      certfile="cert.pem",
+      ssl_version=ssl.PROTOCOL_TLSv1_2
+    )
+    httpd.serve_forever()
+
+  return
+
+
+if __name__ == "__main__":
+  main()
---
+++ temporal/seasonalize.py
@@ -0,0 +1,136 @@
+# seasonalize.py
+# Trevor Pottinger
+# Fri Mar  4 20:42:12 PST 2022
+
+import argparse
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.fftpack
+
+# Default figsize is [6.4, 4.8]
+EXTRA_WIDE = [12.8, 4.8]
+
+# Note: idct of type 2 == dct of type 3
+# np.flip reverses the order of the np array
+# dropna is to drop NaN values
+
+def seasonalize(n: int, arr: np.array) -> np.array:
+  """seasonalize treats `arr` as a row indexed array. Each row
+  is assumed to have columns of values."""
+  wrapped = False
+  if len(arr.shape) == 1:
+    arr = np.array([arr])
+    wrapped = True
+  max_i = np.argmax(arr, axis=1)
+  # np.roll does not play nicely with multiple axis
+  # rotated = np.roll(arr, -max_i, axis=1)
+  rotated = np.copy(arr)
+  for row_i in range(arr.shape[0]):
+    rotated[row_i, 0:(arr.shape[1] - max_i[row_i])] = arr[row_i, max_i[row_i]:]
+    rotated[row_i, (arr.shape[1] - max_i[row_i]):] = arr[row_i, 0:max_i[row_i]]
+  freqs = scipy.fftpack.dct(rotated, type=2, norm=None, axis=1)
+  top_freqs = np.flip(np.argsort(np.abs(freqs), axis=1), axis=1)
+  for row_i in range(arr.shape[0]):
+    for i in range(n, arr.shape[1]):
+      freqs[row_i][top_freqs[row_i][i]] = 0
+  normalized = 1.0 / (2 * arr.shape[1]) * scipy.fftpack.idct(freqs, type=2, norm=None, axis=1)
+  # return np.roll(normalized, max_i)
+  unrotated = np.copy(normalized)
+  for row_i in range(arr.shape[0]):
+    unrotated[row_i, max_i[row_i]:] = normalized[row_i, 0:(arr.shape[1] - max_i[row_i])]
+    unrotated[row_i, 0:max_i[row_i]] = normalized[row_i, (arr.shape[1] - max_i[row_i]):]
+  if wrapped:
+    return unrotated[0]
+  return unrotated
+
+
+def main() -> None:
+  plt.close("all")
+
+  parser = argparse.ArgumentParser(description="Basic deseasonalizer")
+  parser.add_argument("input_file", help="The input CSV file. Use \"-\" for stdin")
+  parser.add_argument("n", type=int, help="The number of frequencies to seasonalize")
+  parser.add_argument("-m", "--metric", default="Volume", help="Which metric to analyze")
+  parser.add_argument("-t", "--ticker", default="MMM", help="Example ticker to test")
+  parser.add_argument("-s", "--since", help="Starting time, inclusive")
+  parser.add_argument("-u", "--until", help="Ending time, exclusive")
+  args = parser.parse_args()
+
+  metrics = ["Volume", "Open", "High", "Low", "Close"]
+  if args.metric not in metrics:
+    print(f"Expected metric, {args.metric}, to be in one of {metrics}")
+    sys.exit(1)
+
+  if args.input_file != "-":
+    df = pd.read_csv(args.input_file)
+  else:
+    df = pd.read_csv(sys.stdin)
+
+  if args.since is not None:
+    df = df[df.Date >= args.since]
+  if args.until is not None:
+    df = df[df.Date < args.until]
+
+  print("summary stats")
+  print(df.describe(include="all"))
+  print(df.head())
+  print(df[["Date", args.metric, "Name"]])
+
+  names = df.Name.value_counts().index.tolist()
+  if args.ticker not in names:
+    print(f"Expected ticker, {args.ticker}, to be in list of Name's, {str(names)}")
+    sys.exit(1)
+
+  print(f"Names: {sorted(names)}")
+  print()
+
+  # TODO figure out why NaN values are showing up
+  series = df[df.Name == args.ticker][args.metric].dropna().to_numpy()
+  print(f"{args.ticker} example")
+  print(series)
+  print(seasonalize(args.n, series))
+  print()
+
+  # History per Name aggregation
+  # hist_lambda = lambda x: pd.Series({"History": list(zip(x.Date, x.Volume))})
+  # hist_df = df.groupby("Name").apply(hist_lambda)
+  # print(hist_df)
+
+  # Values per Date aggregation
+  date_lambda = lambda x: pd.Series({"Values": dict(zip(x.Name, x[args.metric]))})
+  date_df = df.groupby("Date").apply(date_lambda)
+  big_df = pd.DataFrame(date_df.Values.values.tolist(), index=date_df.index)
+  big_df = big_df.dropna(axis=0, how="any")
+  # Now big_df.transpose() == hist_df from above
+  print("big_df and it's transpose")
+  print(big_df)
+  print(big_df.transpose().values)
+  print()
+
+  print("big result")
+  norm_arr = seasonalize(args.n, big_df.transpose().values)
+  norm_df = pd.DataFrame(np.transpose(norm_arr), index=big_df.index, columns=big_df.columns)
+  print(norm_arr)
+  print(norm_df)
+
+  axes = big_df[[args.ticker]].plot(figsize=EXTRA_WIDE)
+  norm_df[[args.ticker]].plot(ax=axes)
+  plt.legend(["big_df", "norm_df"])
+  plt.show()
+
+  # Is `x` a row, a column, or something else?
+  # hist_lambda = lambda x: pd.Series({"History": list(zip(x.Date, x.Volume))})
+  # df = df[df.Date < "2007-01-01"]
+  # hist_df = df.groupby("Name").apply(hist_lambda)
+  # big_df = df.join(hist_df, on="Name", how="left")
+  # Tried:
+  # big_df.apply(lambda row: pd.Series({"Date": row.Date, "Volume": row.Volume, "History": list(filter(lambda x: x[0] < row.Date, row.History))}), axis=1)
+  # big_df.apply(lambda row: pd.Series({**row, **{"History": list(filter(lambda x: x[0] < row.Date, row.History))}}), axis=1)
+  return
+
+
+if __name__ == "__main__":
+  main()
