bd9c4023365cbcaac5acc59716e92325b6b683d5
Trevor Pottinger <trevor@Nehas-MacBook-Air.local>
2024-04-09 14:15:28 -1000

    add some basic components-ish

--- silentdisco2/silentdisco/src/App.js
+++ silentdisco2/silentdisco/src/App.js
@@ -1,15 +1,70 @@
+import { useState } from 'react';
+
 import './App.css';
 
+// TODO App should be called PartyList
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Party List</p>
-	<p>* TODO</p>
-	<p>TODO if (roles.includes "host") { "Create New Party" }</p>
-      </header>
-    </div>
-  );
+  const [partyID, setPartyID] = useState(null);
+  const [participantID, setParticipantID] = useState(null);
+  const [listParty, setListParty] = useState(null);
+
+  // TODO nowPlaying, so we know what song/video is playing
+  // TODO DJ's name... idk if there's multiple DJs
+  // TODO my roles... listener (everyone...), host, DJ
+  // TODO my name
+
+  if (partyID == null) {
+    // <PartyList>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Party List</p>
+          <p>* <button onClick={() => setPartyID("todoXY")}>TODO</button></p>
+          <p>TODO if (roles.includes "host") { "Create New Party" }</p>
+        </header>
+      </div>
+    );
+
+  } else if (participantID != null) {
+    // <Participant>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Name: No Name // TODO</p>
+          <p>Roles: // TODO</p>
+          <p>TODO if (roles.includes "host") "Invite to DJ"</p>
+          <p><button onClick={() => setParticipantID(null)}>Participants list</button></p>
+        </header>
+      </div>
+    );
+
+  } else if (listParty ?? false) {
+    // <ParticipantList>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p><button onClick={() => setParticipantID("abcdef")}>No name</button></p>
+          <p><button onClick={() => setListParty(null)}>Now Playing</button></p>
+        </header>
+      </div>
+    );
+
+  } else {
+    // <NowPlaying>
+    // TODO if roles includes "dj" then replace "Leave Party" button with "Stop DJ"
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Welcome to {partyID}</p>
+          <p>Now playing: TODO</p>
+          <p>My name: TODO</p>
+          <p><button onClick={() => setListParty(true)}>Participants list</button></p>
+          <p><button onClick={() => setPartyID(null)}>Leave Party</button></p>
+        </header>
+      </div>
+    );
+
+  }
 }
 
 export default App;
