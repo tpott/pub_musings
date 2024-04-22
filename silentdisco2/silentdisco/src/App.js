264dfbad29ef6ebb5d310a9bf4e680a3529b47fb
Trevor Pottinger <tpott3@gmail.com>
2024-04-21 23:09:20 -0700

    its starting to look good!

--- silentdisco2/silentdisco/src/App.js
+++ silentdisco2/silentdisco/src/App.js
@@ -59,6 +59,10 @@ function App() {
         return;
       }
 
+	  // This is necessary otherwise we may have loaded the browser with an old git repo
+      console.log('clearing the fs');
+      indexedDB.deleteDatabase('fs');
+
       // window.location.pathname == '/party/:partyID'
       const fs = new FS('fs');
       await git.init({ fs, dir: window.location.pathname });
@@ -82,32 +86,6 @@ function App() {
 
       const files = await git.listFiles({ fs, dir: window.location.pathname });
       console.log(files);
-
-/*
-      // I couldn't find a great way to clear browser data... If you're inspecting the site
-      // then click the "Application" tab, then "Storage" on the left and then "Clear site data".
-      // That should clear the IndexedDB data that backs lightning FS.
-      await fs.promises.writeFile(window.location.pathname + '/now_playing.txt', 'hey\n# start\n');
-      await git.add({ fs, dir: window.location.pathname, filepath: 'now_playing.txt'});
-      const sha = await git.commit({
-        fs,
-        dir: window.location.pathname,
-        author: {
-          name: 'Ron Weasley',
-          email: 'ron@weasly.com',
-        },
-        message: 'lolz',
-      });
-      console.log('done committing', sha);
-      const pushResult = await git.push({
-        fs,
-        http,
-        dir: window.location.pathname,
-        remote: 'origin',
-        ref: 'trunk',
-      });
-      console.log('done pushing', pushResult);
-*/
 
     };
 
--- silentdisco2/silentdisco/src/NowPlaying.js
+++ silentdisco2/silentdisco/src/NowPlaying.js
@@ -1,3 +1,6 @@
+import * as git from 'isomorphic-git';
+import http from "isomorphic-git/http/web";
+import FS from '@isomorphic-git/lightning-fs';
 import { useEffect, useState } from 'react';
 
 import './Party.css';
@@ -6,10 +9,41 @@ function NowPlaying({ partyID, partyRedirect, setListParty }) {
   // TODO use the react dom elements from state?
   const [audioList, setAudioList] = useState([]);
   const [playingList, setPlayingList] = useState([]);
+  const [fs, setFS] = useState(null);
 
   useEffect(() => {
     setAudioList(["e_J14fbBluE.mp3"]);
     setPlayingList([false]);
+
+    // window.location.pathname == '/party/:partyID'
+    const myFs = new FS('fs');
+    console.log('done initializing fs');
+    setFS(myFs);
+
+    // TODO set a reasonable interval for pulling git
+    const intervalId = setInterval(async () => {
+	  // slice is because iso git apparently wants relative paths
+      const gitStatus = await git.status({
+        fs: myFs,
+        dir: window.location.pathname,
+        filepath: window.location.pathname.slice(1),
+      });
+      console.log('pulling', gitStatus);
+      await git.pull({
+        fs: myFs,
+        http,
+        dir: window.location.pathname,
+        remote: 'origin',
+        ref: 'trunk',
+        author: {
+          name: 'Ron Weasley',
+          email: 'ron@weasly.com',
+        },
+      });
+      console.log('done pulling');
+    }, 2000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   // TODO DJ's name... idk if there's multiple DJs
@@ -17,10 +51,40 @@ function NowPlaying({ partyID, partyRedirect, setListParty }) {
   // TODO my name
 
   const playOrPause = (actionType, i) => {
-    return () => {
+    return async () => {
       // TODO use the react dom elements from state?
       const audios = document.getElementsByTagName('audio');
       console.log('clicked', actionType, audioList[i], audios[i].currentTime, (new Date()).getTime() / 1000);
+      if (fs == null) {
+        return;
+      }
+
+      const fileBytes = await fs.promises.readFile(window.location.pathname + '/now_playing.txt');
+      // TODO figure out time skew for scheduling in the future...
+      await fs.promises.writeFile(
+        window.location.pathname + '/now_playing.txt',
+        `(${actionType}, ${audioList[i]}, ${audios[i].currentTime}, ${(new Date()).getTime() / 1000})\n` + fileBytes,
+      );
+      await git.add({ fs, dir: window.location.pathname, filepath: 'now_playing.txt'});
+      const sha = await git.commit({
+        fs,
+        dir: window.location.pathname,
+        author: {
+          name: 'Ron Weasley',
+          email: 'ron@weasly.com',
+        },
+        message: 'dj click',
+      });
+      console.log('done committing', sha);
+      const pushResult = await git.push({
+        fs,
+        http,
+        dir: window.location.pathname,
+        remote: 'origin',
+        ref: 'trunk',
+      });
+      console.log('done pushing', pushResult);
+
       setTimeout(
         () => {
           setPlayingList(playingList.map((_, k) => (actionType === "play" && i === k)));
