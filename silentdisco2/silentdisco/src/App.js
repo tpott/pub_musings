6f87cda9036eea4afdce3f114f80f04c1e343214
Trevor Pottinger <tpott3@gmail.com>
2024-06-29 22:16:24 -0700

    add a linear backoff

--- silentdisco2/silentdisco/src/App.js
+++ silentdisco2/silentdisco/src/App.js
@@ -17,7 +17,7 @@ const doNothing = () => {};
 
 const gitIntervalMs = 16000; // 16 seconds
 const minOffsetChangeInSec = 0.05; // 50 milliseconds
-const reconnectTime = 2000; // 2 seconds
+const reconnectTimeout = 2000; // 2 seconds * connectCount
 
 // TODO don't hardcode this from server.js
 const hostIsTrue = new Uint8Array([104, 111, 115, 116, 58, 116, 114, 117, 101]);
@@ -106,6 +106,7 @@ function App() {
   // TODO generalize this to more roles
   const [isHost, setIsHost] = useState(false);
   const [wsClient, setWSClient] = useState(null);
+  const [connectCount, setConnectCount] = useState(0);
   // a positive offset (> 0) means this device is ahead of the server
   // a negative offset (< 0) means this device is behind the server
   const [offsetInSec, setOffsetInSec] = useState(null);
@@ -198,10 +199,15 @@ function App() {
     const client = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
     client.onopen = () => {
       console.log('WebSocket Client Connected', client);
+      setConnectCount(0);
     };
     client.onclose = () => {
       console.log('WebSocket Client Disconnected');
-      setWSClient(null);
+      // don't create a new WebSocket client here. Let react do it
+      setTimeout(() => {
+        setWSClient(null);
+      }, connectCount * reconnectTimeout);
+      setConnectCount(connectCount + 1);
     };
     // don't set client.onmessage here. we need asyncPullGit for that
     setWSClient(client);
@@ -211,7 +217,7 @@ function App() {
       // memory leaks
       // client.close();
     };
-  }, [wsClient]);
+  }, [wsClient, connectCount]);
 
   useEffect(() => {
     console.log('going to initialize App.js...');
